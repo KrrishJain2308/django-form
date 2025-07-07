c5b554c1756bc01c0e35e86758dbc24bf44c47a8
krrishjain <krrishjain2308@gmail.com>
2025-07-07 23:35:52 +0530

    store data and send email to user

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import ApplicationForms
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -11,5 +14,18 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-            print(first_name)
+
+            Form.objects.create(first_name=first_name,
+                                last_name=last_name,
+                                email=email,
+                                date=date,
+                                occupation=occupation)
+            message_body = f"New job application was submitted. Thank you, {first_name} {last_name}."
+            email_message = EmailMessage("Form submission confirmation", message_body, to= [email])
+            email_message.send()
+
+            messages.success(request,"Form submitted successfully.")
+
+
+
     return render(request,"index.html")
